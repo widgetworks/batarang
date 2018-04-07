db2fc3b28d1a9ca4e05907edb4a5d6dc73ddb6ef
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2018-04-07 17:31:08 +1000

    feat(scopes): add keyboard handling to scopes tab
    Use arrow keys to navigate between scopes in the tree. See #306

--- panel/components/scope-tree/scope-tree.js
+++ panel/components/scope-tree/scope-tree.js
@@ -1,8 +1,8 @@
 angular.module('batarang.scope-tree', []).
 
-directive('batScopeTree', ['$compile', batScopeTreeDirective]);
+directive('batScopeTree', ['$compile', '$document', batScopeTreeDirective]);
 
-function batScopeTreeDirective($compile) {
+function batScopeTreeDirective($compile, $document) {
   return {
     restrict: 'E',
     terminal: true,
@@ -47,31 +47,42 @@ function batScopeTreeDirective($compile) {
       }
       var elt = map[id] = newBranchElement(id);
       var parentElt = map[parentId] || element;
-
+      
+      // wiwo: associate the scope id with the displayed element.
+      elt.data('scopeId', id);
+      
       elt.children().eq(1).on('click', function () {
         scope.$apply(function () {
-          scope.$emit('inspected-scope:change', {
-            id: id
-          });
-          selectedElt.children().eq(0).removeClass('selected');
-          selectedElt.children().eq(1).removeClass('selected');
-
-          selectedElt = elt;
-
-          selectedElt.children().eq(0).addClass('selected');
-          selectedElt.children().eq(1).addClass('selected');
+          inspectScope(id);
+          selectItem(elt);
         });
       });
 
       parentElt.append(elt);
     }
-
+    
     function renderScopeDescriptorElement (id, descriptor) {
       var elt = map[id];
       if (!elt) {
         return;
       }
       elt.children().eq(1).children().eq(1).html(descriptor);
+    }
+    
+    function inspectScope(id){
+      scope.$emit('inspected-scope:change', {
+        id: id
+      });
+    }
+    
+    function selectItem(elt){
+      selectedElt.children().eq(0).removeClass('selected');
+      selectedElt.children().eq(1).removeClass('selected');
+
+      selectedElt = elt;
+
+      selectedElt.children().eq(0).addClass('selected');
+      selectedElt.children().eq(1).addClass('selected');
     }
 
     // TODO: also destroy elements corresponding to descendant scopes
@@ -83,6 +94,133 @@ function batScopeTreeDirective($compile) {
       }
       delete map[id];
     });
+    
+    
+    var $ = angular.element;
+    $document.on('keydown', keyHandler);
+    scope.$on('$destroy', function(){
+      $document.off('keydown', keyHandler);
+    });
+    
+    function keyHandler(event){
+      var keyCode = event.keyCode;
+      if (!selectedElt.length){
+        keyCode = 36; // Equivalent to pressing "home" - select the first item
+      }
+      
+      var elt = selectedElt[0];
+      var eltParent = selectedElt[0];
+      
+      var nextEl;
+      switch (keyCode){
+          case 35: // end
+            nextEl = getLast(element[0]);
+          break;
+          
+          case 36: // home
+            nextEl = findDown(element[0], elt, 1);
+          break;
+          
+          case 37: // left
+            event.preventDefault();
+            nextEl = elt.parentElement;
+          break;
+        
+          case 38: // up
+            event.preventDefault();
+            eltParent = elt.parentElement;
+            
+            nextEl = findUp(eltParent, elt, -1);
+            break;
+            
+          case 40: // down
+            event.preventDefault();
+            direction = 1;
+            nextEl = findDown(eltParent, elt, 1);
+            break;
+      }
+      
+      nextEl = $(nextEl);
+      if (nextEl.length && nextEl.hasClass('children')){
+        // wiwo: associate the scope id with the displayed element.
+        let scopeId = nextEl.data('scopeId');
+        inspectScope(scopeId);
+        selectItem(nextEl);
+      }
+    }
+    
+    var indexOf = Array.prototype.indexOf;
+    function findUp(eltParent, selectedElt, direction){
+      /*
+      start at elt
+       - query for .children
+         - if len > 0: 
+           - get index of `selectedElt` in parent
+           - get element at `index + direction` in elt children list -> return as result
+         - else: 
+           - recursively call `findNext` passing `elt.parent` as first param, `elt` as second param
+      */
+      var children = eltParent.querySelectorAll(':scope > .children');
+      
+      var index = 0;
+      if (eltParent != selectedElt){
+        index = indexOf.call(children, selectedElt);
+      }
+      var nextIndex = index + direction;
+      
+      if (nextIndex < 0 ){
+        result = selectedElt.parentElement;
+      } else if (nextIndex < children.length){
+        // Recursively get the last child in this tree.
+        result = getLast(children[nextIndex]);
+      } else if (eltParent.parentElement && eltParent.parentElement.classList.contains('children')) {
+        result = findUp(eltParent.parentElement, eltParent, direction);
+      }
+      
+      return result;
+    }
+    
+    
+    function getLast(el){
+      var result = el;
+      
+      var children = el.querySelectorAll(':scope > .children');
+      if (children.length){
+        result = getLast(children[children.length - 1]);
+      }
+      return result;
+    }
+    
+    
+    function findDown(eltParent, selectedElt, direction){
+      /*
+      start at elt
+       - query for .children
+         - if len > 0: 
+           - get index of `selectedElt` in parent
+           - get element at `index + direction` in elt children list -> return as result
+         - else: 
+           - recursively call `findNext` passing `elt.parent` as first param, `elt` as second param
+      */
+      var children = eltParent.querySelectorAll(':scope > .children');
+      
+      var nextIndex = 0;
+      if (eltParent != selectedElt){
+        var index = indexOf.call(children, selectedElt);
+        nextIndex = index + direction;
+      }
+      
+      if (nextIndex < 0 ){
+        result = selectedElt.parentElement;
+      } else if (nextIndex < children.length){
+        result = children[nextIndex];
+      } else if (eltParent.parentElement && eltParent.parentElement.classList.contains('children')) {
+        result = findDown(eltParent.parentElement, eltParent, direction);
+      }
+      
+      return result;
+    }
+    
 
   }
 }
